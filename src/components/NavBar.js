4bb79231d208cca568cd056c4128506de4162c94
Ronny <ronnybet41@gmail.com>
2022-09-23 09:53:14 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <nav>NavBar
+  <a href="#Home">Home</a>
+  <a href="#About">About</a>
+  </nav>
+  )
+
 }
 
 export default NavBar;
